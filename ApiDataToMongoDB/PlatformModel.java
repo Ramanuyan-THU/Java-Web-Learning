94aa4d3bd0845e99fe1192116cca8068c94e9bb0
Daniel <daniel2014thu@gmail.com>
2018-04-27 16:34:08 +0800

    Add files via upload

---
+++ ApiDataToMongoDB/PlatformModel.java
@@ -0,0 +1,95 @@
+package testMongoDB;
+
+public class PlatformModel {
+	
+	private String mlat;
+
+	private String glon;
+
+	private String road;
+
+	private String m100;
+	private String nid;
+	private String name;
+	private String glat;
+	private String lon;
+	private String m1000;
+	private String mlon;
+	private String lat;
+	private String direction;
+	
+	
+	public String getMlat() {
+		return mlat;
+	}
+	public void setMlat(String mlat) {
+		this.mlat = mlat;
+	}
+	public String getGlon() {
+		return glon;
+	}
+	public void setGlon(String glon) {
+		this.glon = glon;
+	}
+	public String getRoad() {
+		return road;
+	}
+	public void setRoad(String road) {
+		this.road = road;
+	}
+	public String getM100() {
+		return m100;
+	}
+	public void setM100(String m100) {
+		this.m100 = m100;
+	}
+	public String getNid() {
+		return nid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGlat() {
+		return glat;
+	}
+	public void setGlat(String glat) {
+		this.glat = glat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	public String getM1000() {
+		return m1000;
+	}
+	public void setM1000(String m1000) {
+		this.m1000 = m1000;
+	}
+	public String getMlon() {
+		return mlon;
+	}
+	public void setMlon(String mlon) {
+		this.mlon = mlon;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	
+}
